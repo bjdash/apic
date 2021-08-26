fe7a6659f973575832bd09107d4f25d2d2b6cc09
Bijaya <bijaya.k.dash@gmail.com>
2021-08-26 12:34:35 +0100

    APP: reset version to 3.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -134,7 +134,7 @@ exports.bump = function () {
     /// Usage:
     /// 1. gulp bump : bumps the package.json and bower.json to the next minor revision.
     ///   i.e. from 0.1.1 to 0.1.2
-    /// 2. gulp bump --version 1.1.1 : bumps/sets the package.json and bower.json to the 
+    /// 2. gulp bump --ver 1.1.1 : bumps/sets the package.json and bower.json to the 
     ///    specified revision.
     /// 3. gulp bump --type major       : bumps 1.0.0 
     ///    gulp bump --type minor       : bumps 0.1.0
@@ -142,7 +142,7 @@ exports.bump = function () {
     ///    gulp bump --type prerelease  : bumps 0.0.1-2
     /// </summary>
     var type = args.type;
-    var version = args.version;
+    var version = args.ver;
     var options = {};
     if (version) {
         options.version = version;
