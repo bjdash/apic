81b1d190aa12c1b27cc2599649d190d46fdc7f51
Bijaya <bijaya.k.dash@gmail.com>
2021-09-07 14:29:48 +0100

    App: Updated to angular 12

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-import { version } from '../../package.json';
+import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
   host: 'https://apic.app/',
-  VERSION: version,
+  VERSION: packageInfo.version,
   PLATFORM: 'WEB',
   CAPTCHA_SITE_KEY: '6Le_RmcUAAAAAMD717-PEB6il8QW3wTcPgbBpnXl',
   GOOGLE_CLIENT_ID: '918023175434-lkfap6a7qi47bcje04hno7t7jadd21rd.apps.googleusercontent.com'
