2970355a5f6be2a182512ff6e04b3fca984ee0a5
root <root@apic-main.us-central1-c.c.apic-182009.internal>
2022-08-16 14:55:01 +0000

    added types for marked

--- src/app/components/common/markdown/markdown.component.ts
+++ src/app/components/common/markdown/markdown.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
-import * as marked from 'marked';
+import {marked} from 'marked';
 
 @Component({
   selector: 'markdown, [markdown]',
