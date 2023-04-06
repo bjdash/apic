ec6e0591360c90ebc8d46fdcd73d7ef17b686b41
Bijaya <bijaya.k.dash@gmail.com>
2023-04-06 18:10:54 +0100

    Added web build

---
+++ dist/web/assets/tester/tester.js
@@ -0,0 +1,154 @@
+//@ts-check
+
+var TEST_RUN_CONTEXT = {
+  envs: { saved: null, inMem: null },
+  logs: [],
+  tests: [],
+  scriptError: null
+}
+var $scriptType, $response, $request, $env;
+
+/**
+ * @param {Object} msg
+ * @param {'prescript' | 'postscript'} msg.type
+ * @param {Object} msg.inMem
+ * @param {string[]} msg.logs
+ * @param {Object[]} msg.tests
+ * @param {Object} msg.scriptError
+ */
+function sendResponseBack(msg) {
+  window.parent.postMessage(msg, '*');
+}
+
+/**
+ * @param {Object} data
+ * @param {'prescript' | 'postscript'} data.type
+ * @param {string} data.script
+ * @param {Object} data.$request
+ * @param {Object} data.$response
+ * @param {Object} data.envs
+ * @param {Object} data.envs.saved
+ * @param {Object} data.envs.inMem
+ */
+function onMessageReceived(data) {
+  TEST_RUN_CONTEXT = {
+    envs: data.envs,
+    logs: [],
+    tests: [],
+    scriptError: null
+  };
+  $scriptType = data.type;
+  $request = { ...data.$request };
+  $response = data.$response;
+  let code = data.script || data.$request[data.type];
+  code = prepareScript(code);
+  $env = { ...data.envs.saved, ...data.envs.inMem };
+  Object.freeze($env);
+  Object.freeze($request);
+  Object.freeze($response)
+
+  try {
+    eval(code);
+  } catch (e) {
+    TEST_RUN_CONTEXT.scriptError = e?.message || e?.stack || e.toString();
+    console.error('error', e);
+    log(`Error: ${e?.message || e?.stack || e.toString()}`);
+  }
+  // reqObj.tests = TESTSX.concat(convertToTestX(TESTS));
+  // reqObj.testsX = TESTSX;
+  sendResponseBack({
+    type: $scriptType,
+    inMem: TEST_RUN_CONTEXT.envs.inMem,
+    logs: TEST_RUN_CONTEXT.logs,
+    tests: TEST_RUN_CONTEXT.tests,
+    scriptError: TEST_RUN_CONTEXT.scriptError
+  });
+}
+
+window.addEventListener('message', function (event) {
+  onMessageReceived(event.data);
+}, false);
+
+function prepareScript(code) {
+  return '(function (){\n' + code + '\n})()';
+}
+
+//Library functions
+function setEnv(key, value) {
+  if (!key)
+    return false;
+  if (!TEST_RUN_CONTEXT.envs.inMem)
+    TEST_RUN_CONTEXT.envs.inMem = {};
+
+  TEST_RUN_CONTEXT.envs.inMem = { ...TEST_RUN_CONTEXT.envs.inMem, [key]: value };
+  return true;
+}
+
+function removeEnv(key) {
+  if (key) {
+    let { [key]: omit, ...rest } = TEST_RUN_CONTEXT.envs.inMem;
+    TEST_RUN_CONTEXT.envs.inMem = rest;
+  }
+}
+function getEnv(key) {
+  return TEST_RUN_CONTEXT.envs.inMem?.[key] || TEST_RUN_CONTEXT.envs.saved?.[key]
+}
+
+//TODO: deprecate these
+// function assertTrue(msg, condition) {
+//     if (condition) {
+//         TESTS[msg] = true;
+//     } else {
+//         TESTS[msg] = false;
+//     }
+// }
+
+// function assertFalse(msg, condition) {
+//     if (!condition) {
+//         TESTS[msg] = true;
+//     } else {
+//         TESTS[msg] = false;
+//     }
+// }
+
+function log() {
+  let args = [...arguments], argsString = []
+
+  for (let i = 0; i < args.length; i++) {
+    try {
+      argsString.push(JSON.stringify(args[i]));
+    } catch (e) {
+      argsString.push(`'Error parsing data. Could not convert to string: ${e.message}`);
+    }
+  }
+  TEST_RUN_CONTEXT.logs.push(argsString.join(', '))
+}
+
+function validateSchema(code) {
+  // @ts-ignore
+  if (!Ajv) return false;
+
+  if (code === undefined) {
+    code = $response.status || undefined;
+  }
+  var valid = false;
+  if (code !== undefined && $request.respCodes) {
+    let codeStr = `${code}`;
+    //code = code.toString();
+    var response = $request.respCodes.find(resp => resp.code == codeStr);
+    //response schema is defined against content type so try to find the schema defined against current response content type header else fallback to application/json or at index 0
+    let schema = response.data.find(respItem => { return (respItem.mime == $response.headers['content-type']) || ($response.headers['content-type']?.indexOf(respItem.mime) == 0) })?.schema;
+    if (!schema) {
+      schema = response.data.find(respItem => respItem.mime === 'application/json')?.schema;
+    }
+    if (!schema) {
+      schema = response.data?.[0]?.schema;
+    }
+
+    if (!schema) return false;
+    // @ts-ignore
+    var validator = new Ajv();
+    valid = validator.validate(schema, $response.data);
+  }
+  return valid;
+}
