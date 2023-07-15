9992023a71334496b8043a5d109d214716ef3c00
Bijaya <bijaya.k.dash@gmail.com>
2023-07-15 23:19:49 +0100

    maniest v3

--- platform_files/extension/popup.js
+++ platform_files/extension/popup.js
@@ -1,29 +1,29 @@
-var appId = chrome.app.getDetails().id;
+var appId = chrome.runtime.id;
 var htmlPage = "/index.html";
 var createNewTab = true;
 
-chrome.tabs.getAllInWindow(null, function (tabs) {
-    tabs.forEach(tab => {
-        if (tab.url.indexOf(appId) > 0) {
-            chrome.tabs.update(tab.id, {
-                active: true
-            });
-            createNewTab = false;
-            window.close();
-        }
-    })
-
-    if (createNewTab) {
-        try {
-            chrome.tabs.create({
-                url: chrome.runtime.getURL(htmlPage + '?id=' + appId)
-            });
-        } catch (e) {
-            browser.tabs.create({
-                url: chrome.runtime.getURL(htmlPage + '?id=' + appId),
-                active: true
-            });
-
-        }
+chrome.tabs.query({ currentWindow: true }, function (tabs) {
+  tabs.forEach(tab => {
+    if (tab.url.indexOf(appId) > 0) {
+      chrome.tabs.update(tab.id, {
+        active: true
+      });
+      createNewTab = false;
+      window.close();
     }
+  })
+
+  if (createNewTab) {
+    try {
+      chrome.tabs.create({
+        url: chrome.runtime.getURL(htmlPage + '?id=' + appId)
+      });
+    } catch (e) {
+      browser.tabs.create({
+        url: chrome.runtime.getURL(htmlPage + '?id=' + appId),
+        active: true
+      });
+
+    }
+  }
 });
